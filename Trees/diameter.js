6fdbc3d67dc3d46620da2e4fbbe83e35361bf112
vishumonk-ey <vkant2005@gmail.com>
2026-06-11 00:24:12 +0530

    Create diameter.js

---
+++ Trees/diameter.js
@@ -0,0 +1,5 @@
+// so diameter --> longest path b/w any two nodes .
+// so it can be said as for any node it would be left height + right height
+/* so for all the node go and calculate the diameter including that as a turning pt , calc lh + rh for that --- but notice when i am trying to calculate the lh and rh  , i also need to calc the lh and rh for subsequent nodes .
+so when i am calculating lh and rh , there only i can have the max value updated .
+*/
